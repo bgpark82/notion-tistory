c8dcefa58115cb7c41d3dfe21075f09cced3f878
peter-alethio <peter@alethio.io>
2022-01-29 23:01:22 +0900

    chore: jest 설정

---
+++ src/test/api/notion/notion.test.js
@@ -0,0 +1,12 @@
+import {notionApi} from "../../../popup/api/notion/notion";
+import {BlockListRequest} from "../../../popup/api/notion/BlockListRequest";
+
+describe("노션 API 테스트", () => {
+
+    test("페이지의 블록 리스트를 조회한다", async () => {
+        const 페이지_블록_아이디 = "02626cf5-1dc3-429f-9f0b-fa2b14837d2b";
+        const 페이지_개수 = 100;
+        const response = await notionApi.list(new BlockListRequest(페이지_블록_아이디, 페이지_개수));
+        expect(response.status).toBe(200);
+    })
+})
