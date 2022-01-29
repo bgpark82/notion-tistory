7dd4bf7fe9d86fe1eb929c56a5e4b29e52a98a92
peter-alethio <peter@alethio.io>
2022-01-29 23:37:36 +0900

    test: 티스토리 업로드 테스트

--- src/test/api/notion/tistory.test.js
+++ src/test/api/notion/tistory.test.js
@@ -3,12 +3,12 @@ import {PostSaveRequest} from "../../../popup/api/tistory/PostSaveRequest";
 
 describe("티스토리 API 테스트", () => {
 
-    test("티스토리에 글을 업로드한다", async () => {
+    test.skip("티스토리에 글을 업로드한다", async () => {
         const 글_제목 = "title";
         const 글_내용 = "content";
         const 블로그_이름 = "bgpark";
         const 토큰 = process.env.TISTORY_ACCESS_TOKEN;
         const response = await tistoryApi.post(new PostSaveRequest(글_제목, 글_내용, 블로그_이름, 토큰));
-        expect(response.status).toBe(200);
+        expect(response.status).toBe(1);
     })
 })
